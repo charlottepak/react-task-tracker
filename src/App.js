5f55a1e76ade6b6de9ee2c3cafd0fc70f68632da
Charlotte Pak <charlottepak@outlook.com>
2021-01-28 18:19:00 -0800

    update tasks to allow lists to display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Fec 5th at 2:30',
+      reminder: true,
+    }
+  ])
+
   return (
     <div className='container'>
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,8 @@
-import Task from './Task'
-
-const Tasks = ({ tasks, onDelete, onToggle }) => {
+const Tasks = ({ tasks }) => {
   return (
     <>
-      {tasks.map((task, index) => (
-        <Task key={index} task={task} onDelete={onDelete} onToggle={onToggle} />
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
       ))}
     </>
   )
